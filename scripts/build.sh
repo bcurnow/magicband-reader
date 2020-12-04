31a677a6fe383b6950e15e85935306eacfdd9739
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-04 14:44:13 +0000

    Removed build args as we no longer need them

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,5 @@ rootDir=$(cd ${rootDir}/.. && pwd)
 imageName=$(cd ${rootDir} && basename $(pwd))
 
 docker image build \
-  --build-arg USER_ID=$(id -u ${USER}) \
-  --build-arg GROUP_ID=$(id -g ${USER}) \
-  --build-arg INPUT_GROUP_ID=$(getent group input | cut -d: -f3) \
-  --build-arg INPUT_GROUP_ID=$(getent group audio | cut -d: -f3) \
   -t ${imageName}:latest  \
   ${rootDir}
