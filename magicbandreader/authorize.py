ce48c695a8989d0745c616ccd6d1dae96c130c54
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-06 03:38:54 +0000

    Changed overall name to be more generic, added connection error handling if the service is down.

--- magicbandreader/authorize.py
+++ magicbandreader/authorize.py
@@ -1,24 +1,30 @@
+import logging
 from urllib.parse import urljoin, quote
 
 import requests
 
 
-class OpenDoorAuthorizer:
+class RfidSecuritySvcAuthorizer:
+    """ Uses the rfid-security-svc to authorize an rfid_id for a named permission. """
     def __init__(self, api_url, api_key, permission='Open Door'):
         self.api_url = api_url
         self.api_key = api_key
         self.permission = permission
         self.url = urljoin(api_url, f'authorized/media/{{}}/perm/{quote(permission)}')
 
-    def authorized(self, event):
+    def authorized(self, rfid_id):
         """
         Checks the supplied event against the rfid-security-svc to see if it is authorized.
         Returns True if authorized.
         Returns False otherwise.
         """
-        response = requests.get(
-            self.url.format(event.id),
-            headers={'X-RFIDSECURITYSVC-API-KEY': quote(self.api_key)},
-            verify=False
-            )
-        return response.status_code == 200
+        try:
+            response = requests.get(
+                self.url.format(rfid_id),
+                headers={'X-RFIDSECURITYSVC-API-KEY': quote(self.api_key)},
+                verify=False
+                )
+            return response.status_code == 200
+        except requests.exceptions.ConnectionError as e:
+            logging.error(f'Unable to connect to {self.url.format(rfid_id)}: {e}.')
+            return False
