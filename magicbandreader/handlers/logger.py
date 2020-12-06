e922c8533305188449eda3f1dfb54ed1850cab2b
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-06 06:25:52 +0000

    Switched priority to be multiples of 10

--- magicbandreader/handlers/logger.py
+++ magicbandreader/handlers/logger.py
@@ -5,16 +5,14 @@ from magicbandreader.handlers import AbstractHandler
 
 class LoggingHandler(AbstractHandler):
     def __init__(self, ctx):
-        super().__init__(priority=100)
+        super().__init__(priority=999)
         self.ctx = ctx
 
     def handle_authorized_event(self, event):
         logging.info(f'{event.id} was authorized for {self.ctx.authorizer.permission}')
 
-
     def handle_unauthorized_event(self, event):
         logging.warn(f'{event.id} was NOT authorized for {self.ctx.authorizer.permission}')
-
 
     def handle_none_event(self, event):
         logging.error('Received an event of None, this should not be possible.')
