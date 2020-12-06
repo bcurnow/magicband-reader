f748f7a5361865586aad24e1d6c41afebb181304
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-06 09:25:18 +0000

    Added support ssl verification

--- magicbandreader/authorize.py
+++ magicbandreader/authorize.py
@@ -6,9 +6,10 @@ import requests
 
 class RfidSecuritySvcAuthorizer:
     """ Uses the rfid-security-svc to authorize an rfid_id for a named permission. """
-    def __init__(self, api_url, api_key, permission='Open Door'):
+    def __init__(self, api_url, api_key, api_ssl_verify=True, permission='Open Door'):
         self.api_url = api_url
         self.api_key = api_key
+        self.api_ssl_verify = api_ssl_verify
         self.permission = permission
         self.url = urljoin(api_url, f'authorized/media/{{}}/perm/{quote(permission)}')
 
@@ -22,7 +23,7 @@ class RfidSecuritySvcAuthorizer:
             response = requests.get(
                 self.url.format(rfid_id),
                 headers={'X-RFIDSECURITYSVC-API-KEY': quote(self.api_key)},
-                verify=False
+                verify=self.api_ssl_verify
                 )
             return response.status_code == 200
         except requests.exceptions.ConnectionError as e:
