b2e7c65b94741e4b93365a816c15f7ecfa2d4a48
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-06 06:24:52 +0000

    Switched to pydub and simpleaudio as they are easier to install and have fewer dependencies and don't require this idiotic environment variable to be set.

--- magicbandreader/handlers/authorization_sound.py
+++ magicbandreader/handlers/authorization_sound.py
@@ -1,26 +1,19 @@
-import os
-
-# Don't print the support info to stdout upon startup
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
-
 from magicbandreader.handlers import AbstractHandler
+from magicbandreader.audio import load_sound, play_sound
 
 
 class AuthorizationSoundHandler(AbstractHandler):
     def __init__(self, ctx):
-        super().__init__(priority=4)
-        pygame.mixer.init()
-        self.authorized_sound = pygame.mixer.Sound(os.path.join(ctx.sound_dir, ctx.authorized_sound))
-        self.authorized_sound.set_volume(ctx.volume_level)
-        self.unauthorized_sound = pygame.mixer.Sound(os.path.join(ctx.sound_dir, ctx.unauthorized_sound))
-        self.unauthorized_sound.set_volume(ctx.volume_level)
+        super().__init__(priority=40)
+        self.authorized_sound = load_sound(ctx, ctx.authorized_sound)
+        self.unauthorized_sound = load_sound(ctx, ctx.unauthorized_sound)
 
     def handle_authorized_event(self, event):
-        self.authorized_sound.play()
+        play_sound(self.authorized_sound)
 
     def handle_unauthorized_event(self, event):
-        self.unauthorized_sound.play()
+        play_sound(self.unauthorized_sound)
+
 
 def register(ctx):
     return AuthorizationSoundHandler(ctx)
