e46260dcf7f82ae7141d0b2b6d7d589e90a51cb7
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-06 03:43:47 +0000

    Got to minimal set of priviledge actions and device maps to allow sound, led and RFID to all work.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,4 +4,4 @@ rootDir=$(dirname $0)
 rootDir=$(cd ${rootDir}/.. && pwd)
 targetDir=$(cd ${rootDir} && basename $(pwd))
 
-docker run -it --group-add $(getent group gpio | cut -d: -f3) --group-add kmem --privileged --device=/dev/input/rfid --device=/dev/snd --device=/dev/gpiomem --mount src="${rootDir}",target=/${targetDir},type=bind ${targetDir}:latest /bin/bash
+docker run -it --privileged --mount src=/dev,target=/dev,type=bind --mount src=${rootDir}/../sounds,target=/sounds,type=bind --mount src="${rootDir}",target=/${targetDir},type=bind ${targetDir}:latest /bin/bash
