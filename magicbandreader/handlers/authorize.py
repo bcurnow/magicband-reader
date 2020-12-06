977d8fa0e9ead35bd26f84d5895ad451769da3dd
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-06 06:25:12 +0000

    Switched priority to be multiples of 10

--- magicbandreader/handlers/authorize.py
+++ magicbandreader/handlers/authorize.py
@@ -1,5 +1,3 @@
-import threading
-
 from magicbandreader.authorize import RfidSecuritySvcAuthorizer
 from magicbandreader.event import EventType
 from magicbandreader.handlers import AbstractHandler
@@ -8,10 +6,9 @@ from magicbandreader.handlers import AbstractHandler
 class AuthorizationHandler(AbstractHandler):
     """ Authorizes an event and sets the type accordingly. This handler should come before any handler that relies on the event type."""
     def __init__(self, ctx, authorizer):
-        super().__init__(priority=1)
+        super().__init__(priority=10)
         self.ctx = ctx
         self.ctx.authorizer = authorizer
-
 
     def handle_event(self, event):
         """ Override the handle_event method from the super class as it won't be authorized yet since we're doing that."""
@@ -23,6 +20,7 @@ class AuthorizationHandler(AbstractHandler):
                 event.type = EventType.UNAUTHORIZED
         else:
             event.type = EventType.NONE
+
 
 def register(ctx):
     return AuthorizationHandler(ctx,
