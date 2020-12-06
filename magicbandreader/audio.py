f037799ddebcd60402940c2870f71c9deb4944a5
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-06 06:29:32 +0000

    initial add

---
+++ magicbandreader/audio.py
@@ -0,0 +1,36 @@
+""" Provides helper functions which wrap pydub (with simpleaudio as the player)."""
+import os
+import threading
+import warnings
+
+# Despite the fact that we are using simpleaudio as our player,
+# pydub will still look for ffmpeg/avconv and ffplay/avplay and not find it
+# This will suppress those warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+    from pydub import AudioSegment
+    from pydub.playback import play
+    from pydub.utils import ratio_to_db
+
+
+def load_sound(ctx, sound):
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+
+        sound = AudioSegment.from_file(os.path.join(ctx.sound_dir, sound))
+        # Adjust the volume on the sound based on user input
+        sound = sound.apply_gain(ratio_to_db(ctx.volume_level))
+        return sound
+
+
+def play_sound(sound, background=False):
+    if background:
+        t = threading.Thread(target=_play_sound, args=(sound,))
+        t.start()
+        return t
+
+    _play_sound(sound)
+
+
+def _play_sound(sound):
+    play(sound)
