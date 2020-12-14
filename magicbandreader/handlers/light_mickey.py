38beb46b510ef9471c0c60aba85ea2af5886f2ae
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-14 23:10:36 +0000

    fixed typo in docstring

--- magicbandreader/handlers/light_mickey.py
+++ magicbandreader/handlers/light_mickey.py
@@ -6,7 +6,7 @@ from magicbandreader.led import LedColor
 
 
 class LightMickeyHandler(AbstractHandler):
-    """ Responsible for lighting up the outer ring and innerr ring with appropriate colors based on authentication status."""
+    """ Responsible for lighting up the outer ring and inner ring with appropriate colors based on authentication status."""
     def __init__(self, ctx):
         super().__init__(priority=30)
         self.ctx = ctx
