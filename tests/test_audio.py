0295d868b4d2ea620b35d14a20338ef2b4c92edf
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-14 17:32:18 +0000

    Switched to config fixture

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,29 +1,21 @@
 import os
-from types import SimpleNamespace
 
 from unittest.mock import patch
 
 from magicbandreader import audio
 
 
-CONFIG = {
-    'sound_dir': '/test/',
-    'volume_level': .75
-    }
-CTX = SimpleNamespace(**CONFIG)
-
-
 @patch('magicbandreader.audio.ratio_to_db')
 @patch('magicbandreader.audio.AudioSegment')
-def test_load_sound(AudioSegment, ratio_to_db):
+def test_load_sound(AudioSegment, ratio_to_db, context):
     expected = AudioSegment.from_file.return_value
     expected.apply_gain.return_value = expected
-    ratio_to_db.return_value = CTX.volume_level
-    actual = audio.load_sound(CTX, 'test')
+    ratio_to_db.return_value = context.volume_level
+    actual = audio.load_sound(context, 'test')
     assert actual == expected
-    AudioSegment.from_file.assert_called_once_with(os.path.join(CTX.sound_dir, 'test'))
-    ratio_to_db.assert_called_once_with(CTX.volume_level)
-    expected.apply_gain.assert_called_once_with(CTX.volume_level)
+    AudioSegment.from_file.assert_called_once_with(os.path.join(context.sound_dir, 'test'))
+    ratio_to_db.assert_called_once_with(context.volume_level)
+    expected.apply_gain.assert_called_once_with(context.volume_level)
 
 
 @patch('magicbandreader.audio.threading')
