16b4e5e46586e7d67f2d6f6bedc13bbece88c48a
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-14 23:11:18 +0000

    Propertly split the testing between __init___ and register

--- tests/handlers/test_authorization_sound.py
+++ tests/handlers/test_authorization_sound.py
@@ -7,9 +7,20 @@ AUTH_SOUND = object()
 UNAUTH_SOUND = object()
 
 
+@patch('magicbandreader.handlers.authorization_sound.load_sound')
+def test_Handler___init__(load_sound, context):
+    load_sound.side_effect = [AUTH_SOUND, UNAUTH_SOUND]
+    h = Handler(context)
+    assert h.priority == 40
+    assert h.authorized_sound == AUTH_SOUND
+    assert h.unauthorized_sound == UNAUTH_SOUND
+    assert load_sound.call_count == 2
+    load_sound.assert_has_calls([call(context, context.authorized_sound), call(context, context.unauthorized_sound)], any_order=False)
+
+
 @patch('magicbandreader.handlers.authorization_sound.play_sound')
 @patch('magicbandreader.handlers.authorization_sound.load_sound')
-def test_handle_authorized_event(load_sound, play_sound, context, auth_event):
+def test_Handler_handle_authorized_event(load_sound, play_sound, context, auth_event):
     h = handler(context, load_sound)
     h.handle_authorized_event(auth_event)
     play_sound.assert_called_once_with(AUTH_SOUND)
@@ -17,7 +28,7 @@ def test_handle_authorized_event(load_sound, play_sound, context, auth_event):
 
 @patch('magicbandreader.handlers.authorization_sound.play_sound')
 @patch('magicbandreader.handlers.authorization_sound.load_sound')
-def test_handle_unauthorized_event(load_sound, play_sound, context, unauth_event):
+def test_Handler_handle_unauthorized_event(load_sound, play_sound, context, unauth_event):
     h = handler(context, load_sound)
     h.handle_unauthorized_event(unauth_event)
     play_sound.assert_called_once_with(UNAUTH_SOUND)
@@ -32,6 +43,13 @@ def test_register(load_sound, context):
     assert h.unauthorized_sound == UNAUTH_SOUND
     assert load_sound.call_count == 2
     load_sound.assert_has_calls([call(context, context.authorized_sound), call(context, context.unauthorized_sound)], any_order=False)
+    assert isinstance(h, Handler)
+
+
+@patch('magicbandreader.handlers.authorization_sound.load_sound')
+def test_register(load_sound, context):
+    load_sound.side_effect = [AUTH_SOUND, UNAUTH_SOUND]
+    h = register(context)
     assert isinstance(h, Handler)
 
 
