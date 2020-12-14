92ae9438f2ea2ca43fca15a240663b725fc3c3ff
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-14 18:20:15 +0000

    flake8 errors

--- tests/handlers/test_authorization_sound.py
+++ tests/handlers/test_authorization_sound.py
@@ -27,6 +27,7 @@ def test_handle_unauthorized_event(load_sound, play_sound, context, unauth_event
 def test_register(load_sound, context):
     load_sound.side_effect = [AUTH_SOUND, UNAUTH_SOUND]
     h = register(context)
+    assert h.priority == 40
     assert h.authorized_sound == AUTH_SOUND
     assert h.unauthorized_sound == UNAUTH_SOUND
     assert load_sound.call_count == 2
