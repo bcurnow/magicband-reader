473689e89a2adcf6575f234023396456c8a69d0e
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-14 18:21:37 +0000

    Changed fixtures to be function (default) scope just in case we ever change an event value.

--- tests/fixtures/event.py
+++ tests/fixtures/event.py
@@ -3,16 +3,16 @@ import pytest
 from magicbandreader.event import Event, EventType
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def auth_event(context):
     return Event('test_id', context, EventType.AUTHORIZED)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def unauth_event(context):
     return Event('test_id', context, EventType.UNAUTHORIZED)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def none_event(context):
     return Event('test_id', context, EventType.NONE)
