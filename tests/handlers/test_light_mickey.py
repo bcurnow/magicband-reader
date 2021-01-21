e3b830b8e4b387a0330f975508d71fcb83d14adf
Brian Curnow <bcurnow@users.noreply.github.com>
2021-01-21 21:49:27 +0000

    Broke out the fade_off function into a separate handler so the the authorization sound plays fully before the light goes off

--- tests/handlers/test_light_mickey.py
+++ tests/handlers/test_light_mickey.py
@@ -1,42 +1,31 @@
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 
 from magicbandreader.handlers.light_mickey import LightMickeyHandler as Handler, register
 from magicbandreader.led import LedColor
 
 
-def test_Handler___init__(context):
-    h = Handler(context)
+def test_Handler___init__(context_with_led_controller):
+    h = Handler(context_with_led_controller)
     assert h.priority == 30
-    assert h.ctx == context
+    assert h.ctx == context_with_led_controller
 
 
 @patch('magicbandreader.handlers.light_mickey.threading')
-def test_Handler_handle_authorized_event(threading, context, auth_event):
-    h = Handler(context)
+def test_Handler_handle_authorized_event(threading, context_with_led_controller, auth_event):
+    h = Handler(context_with_led_controller)
     t = threading.Thread.return_value
     h.handle_authorized_event(auth_event)
-    threading.Thread.assert_called_once_with(target=h._light_mickey, args=(LedColor.GREEN,))
+    threading.Thread.assert_called_once_with(target=context_with_led_controller.led_controller.fade_on, args=(LedColor.GREEN,))
     t.start.assert_called_once()
 
 
 @patch('magicbandreader.handlers.light_mickey.threading')
-def test_Handler_handle_unauthorized_event(threading, context, unauth_event):
-    h = Handler(context)
+def test_Handler_handle_unauthorized_event(threading, context_with_led_controller, unauth_event):
+    h = Handler(context_with_led_controller)
     t = threading.Thread.return_value
     h.handle_unauthorized_event(unauth_event)
-    threading.Thread.assert_called_once_with(target=h._light_mickey, args=(LedColor.BLUE,))
+    threading.Thread.assert_called_once_with(target=context_with_led_controller.led_controller.fade_on, args=(LedColor.BLUE,))
     t.start.assert_called_once()
-
-
-@patch('magicbandreader.handlers.light_mickey.time')
-def test_Handler__light_mickey(time, context):
-    led = Mock()
-    context.led_controller = led
-    h = Handler(context)
-    h._light_mickey(LedColor.RED)
-    led.fade_on.assert_called_once_with(LedColor.RED)
-    time.sleep.assert_called_once_with(1)
-    led.fade_off.assert_called_once()
 
 
 def test_register(context):
