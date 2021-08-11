2b44b26e4e1021db9cf85427c9c59d4eed00f8e8
“Brian <bcurnow@users.noreply.github.com>
2021-08-11 13:50:58 -0400

    Added debug logging

--- magicbandreader/handlers/event_type.py
+++ magicbandreader/handlers/event_type.py
@@ -1,3 +1,5 @@
+import logging
+
 from magicbandreader.authorize import RfidSecuritySvcAuthorizer
 from magicbandreader.event import EventType
 from magicbandreader.handlers import AbstractHandler
@@ -13,13 +15,16 @@ class EventTypeHandler(AbstractHandler):
 
     def handle_event(self, event):
         """ Override the handle_event method from the super class as it won't be authorized yet since we're doing that."""
-
+        logging.debug(f'Processing event: {event}')
         if event.id:
             if self.ctx.authorizer.authorized(event.id):
+                logging.debug('Updating event type to AUTHORIZED')
                 event.type = EventType.AUTHORIZED
             else:
+                logging.debug('Updating event type to UNAUTHORIZED')
                 event.type = EventType.UNAUTHORIZED
         else:
+            logging.debug('Updating event type to NONE')
             event.type = EventType.NONE
 
 
